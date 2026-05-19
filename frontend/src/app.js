a100a53e5a8792be0063a7a700ed1b7a08dd01ad
gilmutdinov_ar <gilmutdinov_ar@lexema.ru>
2026-05-19 20:54:38 +0500

    migration for Vue

--- frontend/src/app.js
+++
@@ -1,4 +0,0 @@
-import { TaskFlowApp } from "./core/TaskFlowApp.js";
-
-const app = new TaskFlowApp(document.getElementById("app"));
-app.init();
--- frontend/src/components/not-found.js
+++
@@ -1,7 +0,0 @@
-export function renderNotFound({ target }) {
-  target.innerHTML = `
-    <h1>404</h1>
-    <p>Страница не найдена</p>
-    <a href="#/login">На главную</a>
-  `;
-}
