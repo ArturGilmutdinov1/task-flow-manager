e91cf86e6e6e7691a6c1bb9bbc86acd5f33ad290
ArturGilmutdinov1 <Artur-08.09.1999@Yandex.ru>
2026-06-30 21:37:14 +0500

    feat: add history

---
+++ frontend/src/components/ModalHistoryWindow.vue
@@ -0,0 +1,29 @@
+<template>
+        <table>
+            <thead>
+                <tr>
+                    <th>Пользователь</th>
+                    <th>Дата</th>
+                    <th>Комментарий</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(history, index) in historyData" :key="index">
+                    <td>пока никто</td>
+                    <td>{{ history.at }}</td>
+                    <td>{{ history.comment }}</td>
+                </tr>
+            </tbody>
+            </table>
+</template>
+
+
+<script setup>
+    const props = defineProps({
+        historyData: Array
+    })
+    
+    const historyData = props.historyData;
+    console.log(historyData);
+    
+</script>
